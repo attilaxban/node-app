98160183002223235ee278e2361e94fcda8a0a09
attilaxban <banxattila@gmail.com>
2024-08-14 14:34:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () =>{
+    const rootElement = document.getElementById('root')
+
+    rootElement.insertAdjacentHTML('beforeend','<h1>Hello World!</h1>')
+})
